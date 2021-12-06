3bd26d69e3cd5ccff38ece1cd99e4c72a2d02f23
kobakazu0429 <kobakazu10@gmail.com>
2021-12-06 22:13:36 +0900

    chore: chown

--- copy_pem_files.sh
+++ copy_pem_files.sh
@@ -1,2 +1,5 @@
 cp /etc/letsencrypt/live/wasm-c-web-server.kaz.dev/privkey.pem ./
 cp /etc/letsencrypt/live/wasm-c-web-server.kaz.dev/fullchain.pem ./
+
+chown ubuntu:ubuntu privkey.pem
+chown ubuntu:ubuntu fullchain.pem
